90844ea3e315686397da5509f39b71ca76b45a20
Austin Turner <paustint@gmail.com>
2016-10-29 18:31:35 -0600

    modified case error in apiLog from routes.js

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@
   var express = require('express');
   var router = express.Router();
   // Models
-  var ApiLog = require('./models/ApiLog');
+  var ApiLog = require('./models/apiLog');
 
   // Controllers
   var CustomerController = require('./controllers/customer');
